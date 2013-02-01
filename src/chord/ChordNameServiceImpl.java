5858186f254b02994cdef0e77987bb029699a3b3
Peter Gabrielsen <peterg@cs.au.dk>
2013-02-01 14:16:26 +0100

    Started developing the distributed chord protocol

--- src/chord/ChordNameServiceImpl.java
+++ src/chord/ChordNameServiceImpl.java
@@ -1,1 +1,83 @@
-asdasd
+package chord;
+import interfaces.*;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class ChordNameServiceImpl implements ChordNameService {
+
+    private InetSocketAddress succ;
+    private InetSocketAddress pred;
+    private int port;
+    private int myKey;
+    boolean isGroupCreator;
+    
+    private HashMap<Integer, ChordObject> data;
+    
+    public ChordNameServiceImpl() {
+        this.myKey = keyOfName(getChordName());
+    }
+
+    public int keyOfName(InetSocketAddress name) {
+        return Math.abs(name.hashCode() * 1073741651 % 2147483647);
+    }
+
+    public void createGroup(int port) {
+        this.port = port;
+        isGroupCreator = true;
+        succ = pred = this.getChordName();
+        run();
+    }
+
+    public void joinGroup(InetSocketAddress knownPeer, int port) {
+        this.port = port;
+        isGroupCreator = false;
+        run();
+    }
+
+    public InetSocketAddress getChordName() {
+        try {
+            return new InetSocketAddress(InetAddress.getLocalHost(), port);
+        } catch (UnknownHostException e) {
+            System.err.println("Could not resolve host name");
+            System.err.println(e);
+        }
+        return null;
+    }
+
+    public void leaveGroup() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public InetSocketAddress succ() {
+        return succ;
+    }
+
+    public InetSocketAddress pred() {
+        return pred;
+    }
+
+    public InetSocketAddress lookup(int key) {
+        if (keyOfName(pred) < key && key <= myKey) {
+            return this.getChordName();
+        } else {
+            // need to use RMI or JMS here... Pros/cons.. This exercise is big
+            return null;
+        }
+    }
+
+    public void run() {
+        ServerSocket connection;
+        try {
+            connection = new ServerSocket(port);
+        } catch (IOException e) {
+            System.err.println("There was a problem during socket creation");
+            System.err.println(e);
+        }
+        while (true) {
+        }
+    }
+
+}
--- src/interfaces/ChordNameService.java
+++ src/interfaces/ChordNameService.java
@@ -1,3 +1,4 @@
+package interfaces;
 import java.net.InetSocketAddress;
 
 /**
@@ -17,74 +18,74 @@ import java.net.InetSocketAddress;
 
 public interface ChordNameService extends Runnable {
 
-	/**
-	 * Compute the key of a given name.  Returns a positive 31-bit
-	 * integer, hased as to be "random looking" even for similar
-	 * names.
-	 */
-        public int keyOfName(InetSocketAddress name);
+    /**
+     * Compute the key of a given name.  Returns a positive 31-bit
+     * integer, hashed as to be "random looking" even for similar
+     * names.
+     */
+    public int keyOfName(InetSocketAddress name);
 
-	/**
-	 * Used by the first group member.  Specifies the port on
-	 * which this founding peer is listening for new peers to join
-	 * or leave.  The name of the foudning peer is its local IP
-	 * address and the given port. Its key is derived from the
-	 * name using the method described above.
-	 *
-	 * @param port The port number on which this founding peer is listening.
-	 */
-	public void createGroup(int port);
-	
-	/**
-	 * Used to join a Chord group. This takes place by contacting
-	 * one of the existing peers of the Chord group.  The new peer
-	 * has the name specified by the local IP address and the
-	 * given port. The key of the new peer is derived from its
-	 * name using the method described above.
-	 * 
-	 * @param port The port number on which the new peer is waiting for peers.
-	 * @param serverAddress The IP address and port of the known peer.
-	 */
-        public void joinGroup(InetSocketAddress knownPeer, int port);
+    /**
+     * Used by the first group member.  Specifies the port on
+     * which this founding peer is listening for new peers to join
+     * or leave.  The name of the founding peer is its local IP
+     * address and the given port. Its key is derived from the
+     * name using the method described above.
+     *
+     * @param port The port number on which this founding peer is listening.
+     */
+    public void createGroup(int port);
 
-	/**
-	 * Returns the name of this peer. May only be called after a
-	 * group has been formed or joined.
-	 */
-        public InetSocketAddress getChordName();
+    /**
+     * Used to join a Chord group. This takes place by contacting
+     * one of the existing peers of the Chord group.  The new peer
+     * has the name specified by the local IP address and the
+     * given port. The key of the new peer is derived from its
+     * name using the method described above.
+     * 
+     * @param port The port number on which the new peer is waiting for peers.
+     * @param knownPeer The IP address and port of the known peer.
+     */
+    public void joinGroup(InetSocketAddress knownPeer, int port);
 
-	/**
-	 * Makes this instance of ChordNameService leave the peer
-	 * group. The other peers should be informed of this and the
-	 * Chord network updated appropriately.
-	 */
-        public void leaveGroup();
+    /**
+     * Returns the name of this peer. May only be called after a
+     * group has been formed or joined.
+     */
+    public InetSocketAddress getChordName();
 
-	/**
-	 * Returns the current successor of this peer. In a singleton
-	 * group, the successor is this instance itself.
-	 */
-	public InetSocketAddress succ();
+    /**
+     * Makes this instance of ChordNameService leave the peer
+     * group. The other peers should be informed of this and the
+     * Chord network updated appropriately.
+     */
+    public void leaveGroup();
 
-	/**
-	 * Returns the current predecessor of this peer. In a
-	 * singleton group, the predecessor is this instance itself.
-	 */
-	public InetSocketAddress pred();
-        
-	/**
-	 * Returns the name of the peer who is currently responsible
-	 * for a given key, according to the topology of the Chord
-	 * network.
-	 *
-	 * @param key The key for which we seek the responsible peer. Must be non-negative.
-	 */
-	public InetSocketAddress lookup(int key);
-    
- 	/**
- 	 * Starts the thread which manages this peers participation in
- 	 * the Chord network.
- 	 */
-	public void run();
-	
+    /**
+     * Returns the current successor of this peer. In a singleton
+     * group, the successor is this instance itself.
+     */
+    public InetSocketAddress succ();
+
+    /**
+     * Returns the current predecessor of this peer. In a
+     * singleton group, the predecessor is this instance itself.
+     */
+    public InetSocketAddress pred();
+
+    /**
+     * Returns the name of the peer who is currently responsible
+     * for a given key, according to the topology of the Chord
+     * network.
+     *
+     * @param key The key for which we seek the responsible peer. Must be non-negative.
+     */
+    public InetSocketAddress lookup(int key);
+
+    /**
+     * Starts the thread which manages this peers participation in
+     * the Chord network.
+     */
+    public void run();
+
 }
